92135c9adf0591efd37b5fb618577068be338d2b
Tiago Ferreira <thdejesusferreira@gmail.comgit config --listgit config --listgit config --list git config --list--list>
2024-02-05 12:55:43 -0300

    Order, Instant, ISO 8601 PART1

--- src/main/java/br/com/universelife/course/entities/User.java
+++ src/main/java/br/com/universelife/course/entities/User.java
@@ -1,12 +1,15 @@
 package br.com.universelife.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 //serialização serve para transformar os arquivos em bytes e possa trafegar
 @Entity
@@ -22,6 +25,8 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	@OneToMany(mappedBy="cliente")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User () {
 		
@@ -74,6 +79,14 @@ public class User implements Serializable {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
+	}
+
+	public void setOrders(List<Order> orders) {
+		this.orders = orders;
 	}
 
 	@Override
